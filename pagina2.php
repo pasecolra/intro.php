c254613fc12810c972ea7b67ef01d58c63a3a702
pasecolra <paulcolmenares@colegioguanenta.edu.co>
2024-07-29 18:11:19 -0500

    ejemplos php

---
+++ pagina2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Operadores PHP</title>
+    </head>
+    <body>
+        <h1>Sistemas</h1>
+        <h2>Guanentá</h2>
+        <hr>
+        <?php
+            $a = 10;
+            $b = 15;
+            echo ("La suma de 10 y 15 es: ".($a+$b)."<br/>");
+            echo ("El residuo de 15 y 10: ".($b%$a)."<br/>");
+            echo ("La variable \$a con incremento: ".(++$a)."<br/>");
+        ?>
+    </body>
+</html>
